9e39f52692d40151bd78d9705d5cf2d22515d1c3
Julio Menendez <juliom@hey.com>
2020-09-28 12:59:50 -0600

    Improves performance of build process.

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,9 @@ module.exports = {
   externals: [nodeExternals()],
   devtool: "source-map",
   resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    extensions: [".js", ".json", ".ts"],
+    symlinks: false,
+    cacheWithContext: false,
   },
   output: {
     libraryTarget: "commonjs2",
@@ -23,9 +25,15 @@ module.exports = {
   module: {
     rules: [
       {
-        // Include ts, tsx, js, and jsx files.
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
+        // Include ts and js files.
+        test: /\.(ts|js)$/,
+        exclude: [
+          [
+            path.resolve(__dirname, "node_modules"),
+            path.resolve(__dirname, ".serverless"),
+            path.resolve(__dirname, ".webpack"),
+          ],
+        ],
         use: [
           {
             loader: "cache-loader",
@@ -38,5 +46,12 @@ module.exports = {
       },
     ],
   },
-  plugins: [new ForkTsCheckerWebpackPlugin()],
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        enabled: true,
+        files: "./src/**/*",
+      },
+    }),
+  ],
 };
